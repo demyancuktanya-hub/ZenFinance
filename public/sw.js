3df2773543741e08db0ac46124785ae6c64ac2f9
demyancuktanya-hub <demyancuk.tanya@gmail.com>
2026-03-08 20:11:15 +0500

    Sync: public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,34 @@
-// Минимальный сервис-воркер для поддержки PWA
+const CACHE_NAME = 'zen-finance-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icon.svg',
+  '/manifest.json'
+];
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
-  // Просто пропускаем запросы, этого достаточно для соответствия критериям установки
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
